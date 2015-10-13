e0b222f8ad284ee4089a9f03bf3f0d52339c9042
bengzz <benjagzz@gmail.com>
2015-10-13 15:10:42 -0500

    Actualización

---
+++ EvaluaMed/Evaluate/EvaluateViewController.h
@@ -0,0 +1,13 @@
+//
+//  EvaluateViewController.h
+//  EvaluaMed
+//
+//  Created by Benjamin Gonzalez on 10/13/15.
+//  Copyright (c) 2015 ITESM. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface EvaluateViewController : ViewController
+
+@end
---
+++ EvaluaMed/Main/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  EvaluaMed
+//
+//  Created by Benjamin Gonzalez on 10/6/15.
+//  Copyright (c) 2015 ITESM. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIImageView *imageIntro;
+@property (strong, nonatomic) IBOutlet UIButton *buttonLogin;
+@property (strong, nonatomic) IBOutlet UITextField *labelUser;
+@property (strong, nonatomic) IBOutlet UITextField *labelPassword;
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonLogin:(id)sender {
+}
+
+@end
