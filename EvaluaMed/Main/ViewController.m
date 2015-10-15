c322b0b08cfe7eacc86e4a6c61bf7be846c610bb
bengzz <benjagzz@gmail.com>
2015-10-15 12:13:27 -0500

    Se agrego el código de Login

--- EvaluaMed/Main/ViewController.m
+++ EvaluaMed/Main/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "SBJson.h"
+#import "EvaluateListViewController.h"
 
 @interface ViewController ()
 @property (strong, nonatomic) IBOutlet UIImageView *imageIntro;
@@ -30,7 +32,84 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void) alertStatus:(NSString *)msg :(NSString *)title
+{
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:title
+                                                        message:msg
+                                                       delegate:self
+                                              cancelButtonTitle:@"Ok"
+                                              otherButtonTitles:nil, nil];
+    
+    [alertView show];
+}
+
 - (IBAction)buttonLogin:(id)sender {
+    @try {
+
+        
+        if([self.labelUser.text isEqualToString:@""] || [self.labelPassword.text isEqualToString:@""] ) {
+            [self alertStatus:@"Please enter both Username and Password" :@"Login Failed!"];
+        } else {
+            NSString *post =[[NSString alloc] initWithFormat:@"username=%@&password=%@",self.labelUser.text,self.labelPassword.text];
+            NSLog(@"PostData: %@",post);
+            
+            NSURL *url=[NSURL URLWithString:@"https://evaluamed.com/jsonlogin.php"];
+            
+            NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+            
+            NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[postData length]];
+            
+            NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+            [request setURL:url];
+            [request setHTTPMethod:@"POST"];
+            [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+            [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+            [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+            [request setHTTPBody:postData];
+            
+            //[NSURLRequest setAllowsAnyHTTPSCertificate:YES forHost:[url host]];
+            
+            NSError *error = [[NSError alloc] init];
+            NSHTTPURLResponse *response = nil;
+            NSData *urlData=[NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+            
+            NSLog(@"Response code: %ld", (long)[response statusCode]);
+            if ([response statusCode] >=200 && [response statusCode] <300)
+            {
+                NSString *responseData = [[NSString alloc]initWithData:urlData encoding:NSUTF8StringEncoding];
+                NSLog(@"Response ==> %@", responseData);
+                
+                SBJsonParser *jsonParser = [SBJsonParser new];
+                NSDictionary *jsonData = (NSDictionary *) [jsonParser objectWithString:responseData error:nil];
+                NSLog(@"%@",jsonData);
+                NSInteger success = [(NSNumber *) [jsonData objectForKey:@"success"] integerValue];
+                NSLog(@"%ld",(long)success);
+                if(success == 1)
+                {
+                    NSLog(@"Login SUCCESS");
+                    [self alertStatus:@"Logged in Successfully." :@"Login Success!"];
+                    [self performSegueWithIdentifier:@"showList" sender:self];
+                    
+                } else {
+                    
+                    NSString *error_msg = (NSString *) [jsonData objectForKey:@"error_message"];
+                    [self alertStatus:error_msg :@"Login Failed!"];
+                }
+                
+            } else {
+                if (error) NSLog(@"Error: %@", error);
+                [self alertStatus:@"Connection Failed" :@"Login Failed!"];
+            }
+        }
+    }
+    @catch (NSException * e) {
+        NSLog(@"Exception: %@", e);
+        [self alertStatus:@"Login Failed." :@"Login Failed!"];
+    }
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
 }
 
 @end
