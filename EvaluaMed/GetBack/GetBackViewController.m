0434f0e64ecd7b78d02b24196a96d158a071bdad
bengzz <benjagzz@gmail.com>
2015-10-28 15:39:43 -0600

    Recuperar contraseña actualizado

--- EvaluaMed/GetBack/GetBackViewController.m
+++ EvaluaMed/GetBack/GetBackViewController.m
@@ -33,68 +33,73 @@
     [alertView show];
 }
 
+-(BOOL)validateEmail:(NSString *)emailStr {
+    NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
+    return [emailTest evaluateWithObject:emailStr];
+}
+
 - (IBAction)buttonSend:(id)sender {
-    @try {
-        
-        
-        if([self.labelUser.text isEqualToString:@""]) {
-            [self alertStatus:@"Please enter both Username" :@"Password Recovery Failed!"];
-        } else {
-            NSString *post =[[NSString alloc] initWithFormat:@"usuario=%@",self.labelUser.text];
-            NSLog(@"PostData: %@",post);
-            
-            NSURL *url=[NSURL URLWithString:@"http://evaluacionqx.com/android/olvido.php"];
-            
-            NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
-            
-            NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[postData length]];
-            
-            NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
-            [request setURL:url];
-            [request setHTTPMethod:@"POST"];
-            [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
-            [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
-            [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
-            [request setHTTPBody:postData];
-            
-            
-            NSError *error = [[NSError alloc] init];
-            NSHTTPURLResponse *response = nil;
-            NSData *urlData=[NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
-            
-            NSLog(@"Response code: %ld", (long)[response statusCode]);
-            if ([response statusCode] >=200 && [response statusCode] <300)
-            {
-                NSString *responseData = [[NSString alloc]initWithData:urlData encoding:NSUTF8StringEncoding];
-                NSLog(@"Response ==> %@", responseData);
+    if ([self validateEmail:self.labelUser.text]) {
+        @try {
+                NSString *post =[[NSString alloc] initWithFormat:@"usuario=%@",self.labelUser.text];
+                NSLog(@"PostData: %@",post);
                 
-                SBJsonParser *jsonParser = [SBJsonParser new];
-                NSDictionary *jsonData = (NSDictionary *) [jsonParser objectWithString:responseData error:nil];
-                NSLog(@"%@",jsonData);
-                NSInteger success = [[[jsonData valueForKey:@"logstatus"] objectAtIndex:0] integerValue];
-                NSLog(@"%ld",(long)success);
-                if(success == 1)
+                NSURL *url=[NSURL URLWithString:@"http://evaluacionqx.com/android/olvido.php"];
+                
+                NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+                
+                NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[postData length]];
+                
+                NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+                [request setURL:url];
+                [request setHTTPMethod:@"POST"];
+                [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+                [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+                [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+                [request setHTTPBody:postData];
+                
+                
+                NSError *error = [[NSError alloc] init];
+                NSHTTPURLResponse *response = nil;
+                NSData *urlData=[NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+                
+                NSLog(@"Response code: %ld", (long)[response statusCode]);
+                if ([response statusCode] >=200 && [response statusCode] <300)
                 {
-                    NSLog(@"Login SUCCESS");
-                    [self alertStatus:@"Password Recovery Successfully." :@"Password Recovery Successfully!"];
-                    [self.navigationController popViewControllerAnimated:YES];
-        
-                } else {
+                    NSString *responseData = [[NSString alloc]initWithData:urlData encoding:NSUTF8StringEncoding];
+                    NSLog(@"Response ==> %@", responseData);
                     
-                    NSString *error_msg = (NSString *) [jsonData objectForKey:@"error_message"];
-                    [self alertStatus:error_msg :@"Password Recovery Failed"];
+                    SBJsonParser *jsonParser = [SBJsonParser new];
+                    NSDictionary *jsonData = (NSDictionary *) [jsonParser objectWithString:responseData error:nil];
+                    NSLog(@"%@",jsonData);
+                    NSInteger success = [[[jsonData valueForKey:@"logstatus"] objectAtIndex:0] integerValue];
+                    NSLog(@"%ld",(long)success);
+                    if(success == 1)
+                    {
+                        [self alertStatus:@"Password Recovery Successfully." :@"Password Recovery Successfully!"];
+                        [self.navigationController popViewControllerAnimated:YES];
+                        
+                    } else {
+                        
+                        NSString *error_msg = (NSString *) [jsonData objectForKey:@"error_message"];
+                        [self alertStatus:error_msg :@"Password Recovery Failed"];
+                    }
+                    
+                } else {
+                    if (error) NSLog(@"Error: %@", error);
+                    [self alertStatus:@"Password Recovery Failed" :@"Password Recovery Failed"];
                 }
-                
-            } else {
-                if (error) NSLog(@"Error: %@", error);
-                [self alertStatus:@"Password Recovery Failed" :@"Password Recovery Failed"];
             }
+        @catch (NSException * e) {
+            NSLog(@"Exception: %@", e);
+            [self alertStatus:@"Password Recovery Successfully." :@"Password Recovery Successfully!"];
+            [self.navigationController popViewControllerAnimated:YES];
         }
+
     }
-    @catch (NSException * e) {
-        NSLog(@"Exception: %@", e);
-        [self alertStatus:@"Password Recovery Successfully." :@"Password Recovery Successfully!"];
-        [self.navigationController popViewControllerAnimated:YES];    }
+    else{
+        [self alertStatus:@"Please enter correctly Username" :@"Password Recovery Failed!"];
+    }
 }
 @end
-
